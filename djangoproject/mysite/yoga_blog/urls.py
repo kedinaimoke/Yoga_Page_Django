7acb4eb3ae33c4e323e0ae79f004c9122f8205f6
kedinaimoke <jkoimoks01@gmail.com>
2022-05-12 11:19:39 +0100

    Completed singlepost under blog.html

--- djangoproject/mysite/yoga_blog/urls.py
+++ djangoproject/mysite/yoga_blog/urls.py
@@ -4,8 +4,6 @@ from . import views
 app_name = 'yoga_blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('home/', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
-    path('post/<int:pk>/', views.blog, name='blog'),
+    path('blog/', views.blog, name='blog'),
+    path('singlepost/<int:pk>/', views.singlepost, name='singlepost'),
 ]
--- djangoproject/mysite/yoga_blog/views.py
+++ djangoproject/mysite/yoga_blog/views.py
@@ -1,22 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
+
 # Create your views here.
 
 
-def post_list(request):
+def blog(request):
     posts = Post.objects.all()
-    return render(request, 'yoga_blog/index.html', {'posts': posts})
+    return render(request, 'yoga_blog/blog.html', {'posts': posts})
 
 
-def home(request):
-    return render(request, 'yoga_blog/index.html', {})
-
-
-def blog(request, pk):
+def singlepost(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'yoga_blog/blog.html', {'post': post})
-
-
-def contact(request):
-    return render(request, 'yoga_blog/contact.html', {})
+    return render(request, 'yoga_blog/singlepost.html', {'post': post})
